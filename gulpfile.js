f127e2ec82767bb9fae2b8647474c1263c8e0a6b
zMeFz <saigakj@inbox.ru>
2017-02-17 17:09:48 +0300

    Start mongo at gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
-var gulp       = require( 'gulp' ),
-    server     = require( 'gulp-develop-server' ),
-    livereload = require( 'gulp-livereload' );
+var gulp        = require( 'gulp' ),
+    server      = require( 'gulp-develop-server' ),
+    livereload  = require( 'gulp-livereload' ),
+    exec        = require( 'child_process' ).exec
  
+function runCommand(command) {
+  return function (cb) {
+    exec(command, function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
+  }
+}
+
 var options = {
     path: './src/server.js'
 };
@@ -10,12 +21,14 @@ var serverFiles = [
     './src/**/*.*'
 ];
  
+gulp.task( 'start-mongo', runCommand('sudo service mongod start'));
+
 gulp.task( 'server:start', function() {
     server.listen( options, livereload.listen );
 });
  
 // If server scripts change, restart the server and then livereload. 
-gulp.task( 'default', [ 'server:start' ], function() {
+gulp.task( 'default', [ 'start-mongo', 'server:start' ], function() {
     
     function restart( file ) {
         server.changed( function( error ) {
